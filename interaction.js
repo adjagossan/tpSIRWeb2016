5be1018555433abd4314e3704869d094cb2e6e48
ADJA GOSSAN <adjagossan@gmail.com>
2016-02-07 14:19:49 +0100

    Drag-n-drop implementation ongoing

--- interaction.js
+++ interaction.js
@@ -1,23 +1,64 @@
-
 // La création d'un Dnd requière un canvas et un interacteur.
 // L'interacteur viendra dans un second temps donc ne vous en souciez pas au départ.
 function DnD(canvas, interactor) {
-	// Définir ici les attributs de la 'classe'
+    // Définir ici les attributs de la 'classe'
+    this.posXinit = 0;
+    this.posYinit = 0;
+    this.posXfinal = 0;
+    this.posYfinal = 0;
+    this.mouseIsPressed = false;
 
-	// Developper les 3 fonctions gérant les événements
+    // Developper les 3 fonctions gérant les événements
+    this.maFctGerantLaPression = function(evt) {
+        var res;
+        switch (evt.which) {
+            case 1:
+                res = getMousePosition(canvas, evt);
+                this.posXinit = res.x;
+                this.posYinit = res.y;
+                this.mouseIsPressed = true;
+                console.log("posXinit:" + this.posXinit);
+                console.log("posYinit:" + this.posYinit);
+                break;
+        }
 
-	// Associer les fonctions précédentes aux évènements du canvas.
-};
+    }.bind(this);
 
+    this.maFctGerantLeDeplacement = function(evt) {
+        if (this.mouseIsPressed) {
+
+        }
+    }.bind(this);
+
+    this.maFctGerantLeRelachement = function(evt) {
+        var res;
+        if (this.mouseIsPressed) {
+            res = getMousePosition(canvas, evt);
+            this.posXfinal = res.x;
+            this.posYfinal = res.y;
+            mouseIsPressed = false;
+            console.log("posXfinal:" + this.posXfinal);
+            console.log("posYfinal:" + this.posYfinal);
+        }
+    }.bind(this);
+
+    // Associer les fonctions précédentes aux évènements du canvas.
+    canvas.addEventListener('mousedown', this.maFctGerantLaPression, false);
+    canvas.addEventListener('mousemove', this.maFctGerantLeDeplacement, false);
+    canvas.addEventListener('mouseup', this.maFctGerantLeRelachement, false);
+
+}
+;
 
 // Place le point de l'événement evt relativement à la position du canvas.
 function getMousePosition(canvas, evt) {
-  var rect = canvas.getBoundingClientRect();
-  return {
-    x: evt.clientX - rect.left,
-    y: evt.clientY - rect.top
-  };
-};
+    var rect = canvas.getBoundingClientRect();
+    return {
+        x: evt.clientX - rect.left,
+        y: evt.clientY - rect.top
+    };
+}
+;
 
 
 
--- model.js
+++ model.js
@@ -1,3 +1,106 @@
 
 // Implémenter ici les 4 classes du modèle.
 // N'oubliez pas l'héritage !
+function Drawing(){
+    this.shapes = new Array();
+    
+    this.addShape = function(shape){
+        if(shape instanceof Shape)
+            this.shapes.push(shape);
+    }.bind(this);
+    
+    this.getForms = function(){
+        return this.shapes;
+    }
+};
+
+Drawing.prototype.paint = function(ctx) {
+    console.log(this.getForms());
+    ctx.fillStyle = '#F0F0F0'; // set canvas' background color
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    this.getForms().forEach(function(eltDuTableau) {
+        // now fill the canvas
+        console.log(Object.prototype.toString.call(eltDuTableau));
+        eltDuTableau.paint(ctx);
+    });
+};
+
+
+function Shape(color, epaisseur){
+    this.color = color;
+    this.epaisseur = epaisseur;
+};
+
+Shape.prototype.paint = function(ctx){
+   ctx.fillStyle = this.color;
+   ctx.lineWidth = this.epaisseur;
+}
+
+function Line(dotAx, dotAy, dotBx, dotBy, epaisseur, color){
+    Shape.call(this, color, epaisseur);
+    
+    this.dotAx = dotAx;
+    this.dotAy = dotAy;
+    this.dotBx = dotBx;
+    this.dotBy = dotBy;
+    
+    this.getInitX = function(){
+        return this.dotAx;
+    }.bind(this);
+    
+    this.getInitY = function(){
+        return this.dotAy;
+    }.bind(this);
+    
+    this.getFinalX = function(){
+        return this.dotBx;
+    }.bind(this);
+    
+    this.getFinalY = function(){
+        return this.dotBy;
+    }.bind(this);
+    
+};
+
+Line.prototype.paint = function(ctx) {
+//TODO Manager color
+    ctx.beginPath();
+    ctx.moveTo(this.getInitX(), this.getInitY());
+    ctx.lineTo(this.getFinalX(), this.getFinalY());
+    ctx.stroke();
+
+};
+
+function Rectangle(posX, posY, width, height, epaisseur, color){
+    Shape.call(this, color, epaisseur);
+    
+    this.posX = posX;
+    this.posY = posY;
+    this.width = width;
+    this.heigth = height;
+    
+    this.getInitX = function(){
+        return this.posX;
+    }.bind(this);
+    
+    this.getInitY = function(){
+        return this.posY;
+    }.bind(this);
+    
+    this.getFinalX = function(){
+        return this.width;
+    }.bind(this);
+    
+    this.getFinalY = function(){
+        return this.heigth;
+    }.bind(this);
+};
+
+Rectangle.prototype.paint = function(ctx) {
+//TODO Manager color
+    ctx.rect(this.getInitX(), this.getInitY(), this.getFinalX(), this.getFinalY());
+    ctx.stroke();
+};
+
+Line.prototype = new Shape();
+Rectangle.prototype = new Shape();
